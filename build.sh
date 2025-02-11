10c1fce36f33f5c53d17a15cbe8d60a0d3f88c21
santos <admin@google.com>
2025-02-11 20:32:56 +0800

    Add support for cloudflared web console version information.

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e  # 发生错误时退出
 
 APP_NAME="cftun"
-VERSION="2.0.0"
-BUILD_TYPE="release"
+VERSION="2.0.1"
+BUILD_TYPE="product"
 BUILD_DIR="build"
 PLATFORMS=("linux/amd64" "linux/arm64" "linux/arm" "windows/amd64" "windows/arm64" "darwin/amd64" "darwin/arm64")
 
